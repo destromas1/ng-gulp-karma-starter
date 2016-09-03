b340b2d3f92ed91d35440af59247e605d16beae7
Shahjada <shahjada.talukdar@gmail.com>
2016-09-04 01:26:04 +0600

    karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function (config) {
+  config.set({
+
+    frameworks: ['jasmine'],
+
+    files: [
+      'bower_components/jquery/dist/jquery.min.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/underscore/underscore.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'app/app.js',
+      'app/controllers/*.js',
+      'app/services/*.js',
+      'test/**/*.spec.js'],
+
+    port: 3001,
+
+    browsers: ['Chrome']
+  });
+};
+
